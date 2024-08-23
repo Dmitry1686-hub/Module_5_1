b98a1e32f23f57c8d9ba0084908b850c7f439f26
Dmitry1686-hub <varchik.333@gmail.com>
2024-08-23 18:42:28 +0300

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -4,16 +4,19 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self,new_floor):
-        for i in range(1, new_floor + 1):
-          self 1 <= new_floor <= self.number_of_floors:
+        for i in  range (1, new_floor + 1):
+           if 1 <= new_floor <= self.number_of_floors:
                 print(i)
-            else:
+           else:
                 print('Такого этажа не существует')
                 break
-
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+h1.go_to(-5)
+h2.go_to(3)
+h1.go_to(0)
+h2.go_to(1)
